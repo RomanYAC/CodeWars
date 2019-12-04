471e3abb081932536ee8f5002f7985b7f89cfe1f
Roman Yatsenko <ryatsenko@exoft.net>
2019-12-04 19:28:32 +0200

    Add project files.

---
+++ CodeWars.Core/Data/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars.Core.Data
+{
+    interface IUnitOfWork
+    {
+    }
+}
---
+++ CodeWars.Core/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace CodeWars.Core
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
